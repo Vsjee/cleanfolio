96ec153864a93c268155cc76662a0fa07c345df4
David Hernandez <davidfelipehernandez1@gmail.com>
2023-12-27 22:44:30 -0500

    add basic header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom"
 import { Home } from "./pages"
+import { Footer, Header } from "./shared"
 
 function App() {
   return (
     <>
+     <Header />
       <Routes>
         <Route path="/" element={ <Home /> } />
       </Routes>
+      <Footer />
     </>
   )
 }
---
+++ src/app/shared/index.ts
@@ -0,0 +1,4 @@
+export { default as Footer } from './components/footer/Footer';
+export { default as Header } from './components/header/Header';
+export * from './components';
+export * from './index';
