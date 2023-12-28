b10ed218c94ba5ad6e731c6138dfb93b362f43f2
David Hernandez <davidfelipehernandez1@gmail.com>
2023-12-27 22:35:42 -0500

    add basic routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,12 @@
+import { Routes, Route } from "react-router-dom"
+import { Home } from "./pages"
 
 function App() {
   return (
     <>
-      App
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+      </Routes>
     </>
   )
 }
